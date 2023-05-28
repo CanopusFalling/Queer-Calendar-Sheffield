4041af6c2cb589e4e01acbd946ddd404caa7d56b
CanopusFalling <kent_sam@outlook.com>
2023-05-28 22:41:14 +0100

    Jazzed the site up with some quick bootstrap, I'll need to do code cleanup to sort this mess out though because this is a quick and dirty solution to the problem.

--- js/get_calendar_info.js
+++ js/get_calendar_info.js
@@ -3,31 +3,69 @@ const GOOGLE_CALENDAR_ID = 'queercalendarsheffield@gmail.com';
 const GOOGLE_CALENDAR_API_KEY = 'AIzaSyCG_DrTXfD1Zrt2rCXsgc5-W0reHuPG49E';
 
 function writeEventsToDOM(events) {
-    var eventsContainer = document.getElementById('calendar-events');
+    var eventsContainer = document.getElementById('calendar-card-container');
+    var modalsContainer = document.getElementById('event-modal-container');
+
 
     // Iterate over the events data array and create HTML div elements for each event
     events.forEach(function (event) {
-        var eventDiv = document.createElement('div');
-        eventDiv.classList.add('event');
+        try{
+            var eventDiv = document.createElement('div');
+            eventDiv.classList.add('col');
+            
+            eventDiv.innerHTML = `
+            <div class="card">
+                <div class="card-header">
+                    <h4 class="card-title">${event.summary}</h4>
+                </div>
+                <div class="card-body">
+                    <p class="card-text">
+                        <span class="fw-bold">Location:</span> ${event.location}
+                    </p>
+                    <p class="card-text">
+                        <span class="fw-bold">Start Time:</span> ${event.start.dateTime}
+                    </p>
+                    <p class="card-text">
+                        <span class="fw-bold">End Time:</span> ${event.end.dateTime}
+                    </p>
+                    <p class="card-text text-truncate">${event.description}</p>
+                    <button type="button" class="btn btn-primary" data-bs-toggle="modal"
+                        data-bs-target="#eventModal-${event.id}">More Information</button>
+                </div>
+            </div>
+            `;
 
-        // Event properties
-        var summary = event.summary;
-        var startDateTime = event.start.dateTime;
-        var endDateTime = event.end.dateTime;
-        var location = event.location;
-        var description = event.description;
+            console.log(eventDiv)
 
-        // Populate the event div with the properties
-        var eventHTML = '<h2>' + summary + '</h2>';
-        eventHTML += '<p><strong>Start:</strong> ' + startDateTime + '</p>';
-        eventHTML += '<p><strong>End:</strong> ' + endDateTime + '</p>';
-        eventHTML += '<p><strong>Location:</strong> ' + location + '</p>';
-        eventHTML += '<p><strong>Description:</strong> ' + description + '</p>';
+            // Append the event div to the container
+            eventsContainer.appendChild(eventDiv);
 
-        eventDiv.innerHTML = eventHTML;
+            modalsContainer.innerHTML = modalsContainer.innerHTML + `
+            <div class="modal fade" id="eventModal-${event.id}" tabindex="-1" aria-labelledby="eventModalLabel-${event.id}"
+                aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="eventModalLabel-${event.id}">${event.summary}</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            <p><span class="fw-bold">Location:</span> ${event.location}</p>
+                            <p><span class="fw-bold">Start Time:</span> ${event.start.dateTime}</p>
+                            <p><span class="fw-bold">End Time:</span> ${event.end.dateTime}</p>
+                            <p>${event.description}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>`
+        }catch(error){
+            console.log("Event Failed: ${error}")
+        }
+    });
 
-        // Append the event div to the container
-        eventsContainer.appendChild(eventDiv);
+    const eventModals = document.querySelectorAll('[id^="eventModal-"]');
+    eventModals.forEach(function (modal) {
+        const bootstrapModal = new bootstrap.Modal(modal);
     });
 }
 
@@ -54,6 +92,6 @@ function fetchCalendarData() {
     });
 }
 
-
-
-window.onload = fetchCalendarData;
+document.addEventListener("DOMContentLoaded", function () {
+    fetchCalendarData();
+})
