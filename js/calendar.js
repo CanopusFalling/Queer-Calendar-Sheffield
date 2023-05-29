87bdd835a9197ceb3add03ccf0c96e027af4ab64
CanopusFalling <kent_sam@outlook.com>
2023-05-29 11:44:41 +0100

    Got the calendar compiling multiple sources and outputting to the test page doccument.

--- js/calendar.js
+++ js/calendar.js
@@ -13,8 +13,10 @@ class EventSource {
     }
 }
 
-const QCS_CALENDAR_SOURCE = new EventSource("Queer Calendar Sheffield Google Calendar", SourceType.google_calendar, "queercalendarsheffield@gmail.com")
-const COMMUNITY_SOURCES = [new EventSource("Peak Queer Adventures Google Calendar", SourceType.google_calendar, "peakqueeradventures@gmail.com")]
+const QCS_CALENDAR_ID = "queercalendarsheffield@gmail.com"; 
+const SOURCES = [
+    new EventSource("Queer Calendar Sheffield Google Calendar", SourceType.google_calendar, "queercalendarsheffield@gmail.com"),
+    new EventSource("Peak Queer Adventures Google Calendar", SourceType.google_calendar, "peakqueeradventures@gmail.com")];
 
 class Event {
     constructor(data, format) {
@@ -23,8 +25,7 @@ class Event {
         }
     }
 
-    #google_constructior(data){
-        //console.log(data)
+    #google_constructior(data) {
         this.title = data.summary
         this.description = data.description
         this.location = data.location
@@ -34,42 +35,99 @@ class Event {
         this.last_updated = data.updated
     }
 
-    #google_ingest_time(data){
-        if(data .start != undefined){
-            if (data.start.dateTime != undefined) { this.start = data.start.dateTime }
-            else if (data.start.date != undefined) { this.start = data.start.date }
-            else this.start = undefined
-        }else{
-            this.start = undefined
+    #google_ingest_time(data) {
+        if (data.start != undefined) {
+            if (data.start.dateTime != undefined) { this.start = new Date(data.start.dateTime) }
+            else if (data.start.date != undefined) { this.start = new Date(data.start.date) }
         }
-        if(data .end != undefined){
-            if (data.end.dateTime != undefined) { this.end = data.end.dateTime }
-            else if (data.end.date != undefined) { this.end = data.end.date }
+
+        if (data.end != undefined) {
+            if (data.end.dateTime != undefined) { this.end = new Date(data.end.dateTime) }
+            else if (data.end.date != undefined) { this.end = new Date(data.end.date) }
             else this.end = undefined
-        }else{
-            this.end = undefined
         }
     }
 
-    toString(){
-        return `${this.title} | ${this.location} | ${this.start} - ${this.end}`
+    formatDate(date) {
+        return date.toLocaleString(undefined, {
+            day: 'numeric',
+            month: 'long',
+            year: 'numeric',
+            hour: 'numeric',
+            minute: 'numeric',
+            hour12: true
+        });
+    }
+
+    formatTime(date) {
+        return date.toLocaleString(undefined, {
+            hour: 'numeric',
+            minute: 'numeric',
+            hour12: true
+        });
+    }
+
+    getTimeString() {
+        if (this.end == undefined) {
+            return this.formatDate(this.start)
+        } else {
+            if (this.end.getDay() == this.start.getDay()) {
+                return this.formatDate(this.start) + " - " + this.formatTime(this.end)
+            } else {
+                return this.formatDate(this.start) + " - " + this.formatDate(this.end)
+            }
+        }
+    }
+
+    toString() {
+        return `${this.title} | ${this.location} | ${this.getTimeString()}`
     }
 }
 
 class Calendar {
-    constructor(primary_source, community_sources, events) {
-        this.primary_source
-        this.community_sources
+    constructor() {
+        this.events = []
+    }
+
+    hasEvent(new_event){
+        this.events.forEach(event => {
+            if(event.name == new_event.name && event.start == new_event.start){
+                return true
+            }
+        })
+        return false
+    }
+
+    inputEvents(events){
+        events.forEach(event => {
+            if(!this.hasEvent(event)){
+                this.events.push(event)
+            }
+        });
+    }
+
+    outputInfo(){
+        //Placeholder, will output all the events to the DOM.
+        document.body.innerHTML = this.events
     }
 }
+
+var loaded_sources = 0;
+
+var page_calendar = new Calendar();
 
 function handleGoogleCalendarData(data) {
     let events = []
     data.items.forEach(event => {
-        events.push(new Event(event, SourceType.google_calendar).toString() + "<br>" )
+        events.push(new Event(event, SourceType.google_calendar))
     });
 
-    document.body.innerHTML = events
+    page_calendar.inputEvents(events)
+
+    loaded_sources += 1;
+    if(loaded_sources == SOURCES.length){
+        page_calendar.outputInfo()
+    }
 }
 
 function fetchCalendarData(calendar, callback, timeMin, timeMax) {
@@ -78,18 +136,22 @@ function fetchCalendarData(calendar, callback, timeMin, timeMax) {
     if (timeMax == undefined) { timeMax = "" } else { timeMax = "&timeMax=" + timeMax }
 
     var requestUrl = `https://www.googleapis.com/calendar/v3/calendars/${calendar.google_calendar_ID}/events?callback=${callback}&key=${GOOGLE_CALENDAR_API_KEY}&showDeleted=False&singleEvents=True&orderBy=starttime${timeMin}${timeMax}`;
-    console.log(requestUrl)
+
     var script = document.createElement('script');
     script.src = requestUrl;
     document.body.appendChild(script);
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-    var page_calendar = new Calendar();
+function loadSources(timeMin, timeMax){
+    SOURCES.forEach(source => {
+        fetchCalendarData(source, "handleGoogleCalendarData", timeMin, timeMax); 
+    });
+}
 
+document.addEventListener("DOMContentLoaded", function () {
     var currentUrl = window.location.href;
     var url = new URL(currentUrl);
     var searchParams = url.searchParams;
 
-    fetchCalendarData(QCS_CALENDAR_SOURCE, "handleGoogleCalendarData", searchParams.timeMin, searchParams.timeMax);
+    loadSources(searchParams.timeMin, searchParams.timeMax)
 })
