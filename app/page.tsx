765d388b1d3415b0d8ccf7cb25abf10e7867462f
CanopusFalling <kent_sam@outlook.com>
2023-06-28 21:06:39 +0100

    Updated to staticly serve the original website.

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,89 @@
-import Image from 'next/image'
+import React from 'react';
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+function HomePage() {
+    const htmlContent = `
+  <!DOCTYPE html>
+
+  <head>
+      <!-- HTML stuff you have to do for legacy reasons I think? 
+          At this point I'm fairly certain my usage of these lines is basically just superstition 
+          but heyo, don't wanna accidentally let any of the bugs in. -->
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+  
+      <!-- Site Information. -->
+      <title>Queer Calendar Sheffield</title>
+      <meta name="description" content="A calendar for queer events happening in the city of Sheffield.">
+      <meta name="author" content="">
+  
+      <meta property="og:title" content="Queer Calendar Sheffield">
+      <meta property="og:type" content="website">
+      <!--<meta property="og:url" content="">-->
+      <meta property="og:description" content="A calendar for queer events happening in the city of Sheffield.">
+      <!--<meta property="og:image" content="example-picture.png">-->
+  
+      <!-- <link rel="icon" href="/favicon.ico"> Legacy Browser Favicon Support. -->
+      <!-- <link rel="icon" href="/favicon.svg" type="image/svg+xml">  Modern SVG Favicon. -->
+      <!-- <link rel="apple-touch-icon" href="/apple-touch-icon.png"> Support for Apple's Random Bullshit. -->
+  
+      <!-- Links to external files. -->
+      <!-- <link rel="stylesheet" href="styles.css"> -->
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+      <script type='text/javascript' src='js/calendar.js'></script>
+  </head>
+  
+  <body>
+      <header>
+          <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+              <div class="container">
+                  <a class="navbar-brand" href="#">Queer Calendar Sheffield</a>
+                  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                      aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                      <span class="navbar-toggler-icon"></span>
+                  </button>
+                  <div class="collapse navbar-collapse" id="navbarNav">
+                      <!-- <ul class="navbar-nav ml-auto">
+                          <li class="nav-item">
+                              <a class="nav-link" href="#">Home</a>
+                          </li>
+                          <li class="nav-item">
+                              <a class="nav-link" href="#">Events</a>
+                          </li>
+                          <li class="nav-item">
+                              <a class="nav-link" href="#">About</a>
+                          </li>
+                          <li class="nav-item">
+                              <a class="nav-link" href="#">Contact</a>
+                          </li>
+                      </ul> -->
+                  </div>
+              </div>
+          </nav>
+      </header>
+  
+      <main>
+          <section id="calendar-events" class="container my-5">
+              <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4" id="event-card-container">
+              </div>
+          </section>
+      </main>
+  
+  
+  
+      <footer class="bg-light text-center py-3">
+          <div class="container">
+              <p><a href="https://github.com/CanopusFalling/Queer-Calendar-Sheffield">view project on github</a></p>
+          </div>
+      </footer>
+  
+      <!-- Modals -->
+      <div id="event-modal-container">
       </div>
+  </body>
+  `;
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    return <div dangerouslySetInnerHTML={{ __html: htmlContent }} />;
 }
+
+export default HomePage;
---
+++ public/js/calendar.js
@@ -0,0 +1,329 @@
+const GOOGLE_CALENDAR_API_KEY = "AIzaSyCG_DrTXfD1Zrt2rCXsgc5-W0reHuPG49E"
+
+/**
+ * @enum {string}
+ * @description ENUM to identify calendar source types.
+ */
+const CalendarSourceType = {
+    GOOGLE_CALENDAR: 'google_calendar'
+};
+
+/**
+ * @class CalendarSource
+ * @classdesc Represents an online calendar that can be used to pull events from.
+ * @constructor
+ * @param {string} name - The name of the calendar source.
+ * @param {CalendarSourceType} type - The type of the calendar source.
+ * @param {string} googleCalendarId - The ID of the Google Calendar associated with this calendar source.
+ */
+class CalendarSource {
+    constructor(name, type, googleCalendarId) {
+        this.name = name;
+        this.type = type;
+        this.googleCalendarId = googleCalendarId;
+    }
+
+    fetchEvents(timeMin, timeMax) {
+        if (this.type == CalendarSourceType.GOOGLE_CALENDAR) {
+            return this.#fetchGoogleCalendarData(timeMin, timeMax)
+        }
+    }
+
+    /**
+     * Function to request information from the google calendar event's API, reference for the API here:
+     * https://developers.google.com/calendar/api/v3/reference/events/list
+     * @param {*} timeMin
+     * @param {*} timeMax 
+     * @param {*} showDeleted 
+     */
+    #fetchGoogleCalendarData(timeMin, timeMax, showDeleted) {
+        let parameters = {
+            key: GOOGLE_CALENDAR_API_KEY,
+            timeMin: timeMin.toISOString() || new Date().toISOString(), //Sets timeMin to current date if one isn't specified.
+            timeMax: timeMax.toISOString(),
+            showDeleted: showDeleted,
+            singleEvents: true
+        };
+
+        let queryString = new URLSearchParams(
+            Object.entries(parameters)
+                .filter(([key, value]) => value !== undefined)
+        ).toString();
+        let requestURL = `https://www.googleapis.com/calendar/v3/calendars/${this.googleCalendarId}/events?${queryString}`
+
+        let events = []
+
+        return fetch(requestURL)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data)
+                let events = data.items.map(event => new CalendarEvent(event, this));
+                return events;
+            })
+            .catch(error => {
+                console.log(error);
+                return [];
+            });
+
+        return events
+    }
+}
+
+/**
+ * @class CalendarEvent
+ * @classdesc Class to represent an event on the calendar.
+ */
+class CalendarEvent {
+    /**
+     * @constructor
+     * @param {Object} data - Raw data about this event from the event source.
+     * @param {CalendarSource} source - Source for the event.
+     */
+    constructor(data, source) {
+        this.source = source
+
+        if (source.type == CalendarSourceType.GOOGLE_CALENDAR) {
+            this.#google_constructior(data)
+        }
+    }
+
+    /**
+     * A function to handle the construction of google calendar objects.
+     * @param {Object} data
+     */
+    #google_constructior(data) {
+        console.log(data)
+
+        // Basic Event Information.
+        this.title = data.summary;
+        this.creator = this.creator;
+        this.description = data.description;
+        this.location = data.location
+        
+        // Calendar ID Information.
+        this.iCalUID = data.iCalUID;
+        this.googleID = data.id
+
+        this.dateCreated = data.created;
+
+        this.#google_ingest_time(data)
+        this.calendar_link = data.htmlLink
+
+        this.last_updated = data.updated
+    }
+
+    /**
+     * A function to handle ingesting time from google events.
+     * @param {Object} data 
+     */
+    #google_ingest_time(data) {
+        if (data.start != undefined) {
+            if (data.start.dateTime != undefined) { this.start = new Date(data.start.dateTime) }
+            else if (data.start.date != undefined) { this.start = new Date(data.start.date) }
+        }
+
+        if (data.end != undefined) {
+            if (data.end.dateTime != undefined) { this.end = new Date(data.end.dateTime) }
+            else if (data.end.date != undefined) { this.end = new Date(data.end.date) }
+            else this.end = undefined
+        }
+    }
+
+    /**
+     * Formats a date into a string
+     * @param {Object} date 
+     * @returns {String} in the following format "DD [MonthName] YYYY at HH:MM [am/pm]"
+     */
+    formatDate(date) {
+        return date.toLocaleString(undefined, {
+            day: 'numeric',
+            month: 'long',
+            year: 'numeric',
+            hour: 'numeric',
+            minute: 'numeric',
+            hour12: true
+        });
+    }
+
+    /**
+     * Formats a date into a string with just the time.
+     * @param {Object} date 
+     * @returns {String} in the following format "HH:MM [am/pm]"
+     */
+    formatTime(date) {
+        return date.toLocaleString(undefined, {
+            hour: 'numeric',
+            minute: 'numeric',
+            hour12: true
+        });
+    }
+
+    /**
+     * Function to get the time as a String for the event.
+     * @returns {String} Time in human readable format.
+     */
+    getTimeString() {
+        if (this.end == undefined) {
+            return this.formatDate(this.start)
+        } else {
+            if (this.end.getDay() == this.start.getDay()) {
+                return this.formatDate(this.start) + " - " + this.formatTime(this.end)
+            } else {
+                return this.formatDate(this.start) + " - " + this.formatDate(this.end)
+            }
+        }
+    }
+
+    /**
+     * Function to return the event as an easily human readable string.
+     * @returns {String} Event title, location and time.
+     */
+    toString() {
+        return `${this.title} | ${this.location} | ${this.getTimeString()}`
+    }
+
+    /**
+     * Output the event as a Card Element.
+     * @returns {String} HTML event card element.
+     */
+    toEventCard() {
+        return `
+        <div class="col">
+            <div class="card">
+                <div class="card-header">
+                    <h4 class="card-title">${this.title}</h4>
+                </div>
+                <div class="card-body">
+                    <p class="card-text">
+                        <span class="fw-bold">Location:</span> ${this.location}
+                    </p>
+                    <p class="card-text">
+                        <span class="fw-bold">Time:</span> ${this.getTimeString()}
+                    </p>
+                    <p class="card-text text-truncate">${this.description}</p>
+                    <button type="button" class="btn btn-primary" data-bs-toggle="modal"
+                        data-bs-target="#eventModal-${this.id}">More Information</button>
+                </div>
+            </div>
+        </div>`
+    }
+
+    /**
+     * Output the event as a Modal Element.
+     * @returns {String} HTML Modal Element
+     */
+    toModal() {
+        return `
+        <div class="modal fade" id="eventModal-${this.id}" tabindex="-1" aria-labelledby="eventModalLabel-${this.id}"
+            aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="eventModalLabel-${this.id}">${this.title}</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <p><span class="fw-bold">Location:</span> ${this.location}</p>
+                        <p><span class="fw-bold">Time:</span> ${this.getTimeString()}</p>
+                        <p>${this.description}</p>
+                    </div>
+                </div>
+            </div>
+        </div>`
+    }
+}
+
+/**
+ * @class Calendar
+ * @classdesc A class to store, organise and output all of the CalendarEvents.
+ */
+class Calendar {
+    constructor(calendarSources) {
+        this.calendarSources = calendarSources;
+
+        this.minDate = new Date();
+        this.maxDate = new Date();
+
+        this.events = new Array();
+    }
+
+    fetchAllEvents(timeMin, timeMax) {
+        timeMin = timeMin || new Date()
+        timeMax = timeMax || new Date(Date.now() + (365 * 24 * 60 * 60 * 1000)) //Sets date to a year from now if one isn't provided.
+
+        let fetchPromises = this.calendarSources.map(async (source) => {
+            let events = await source.fetchEvents(timeMin, timeMax);
+            this.events.push(...events);
+        });
+
+        return Promise.all(fetchPromises);
+    }
+
+    /**
+     * Checks if an event is already in the calendar.
+     * The check might have to compare from different sources with competing info.
+     * @param {CalendarEvent} new_event 
+     * @returns {Boolean}
+     */
+    hasEvent(new_event) {
+        this.events.forEach(event => {
+            if (event.name == new_event.name && event.start == new_event.start) {
+                return true
+            }
+        })
+        return false
+    }
+
+    /**
+     * Sorts all the events in the calendar by start date.
+     */
+    sortEvents() {
+        this.events.sort(function (a, b) {
+            return a.start - b.start;
+        });
+    }
+
+    /**
+     * Outputs all the calendar events into the DOM.
+     */
+    outputInfo() {
+        this.sortEvents();
+
+        let event_container = document.getElementById(EVENT_CARD_CONTAINER_ID)
+        let modal_container = document.getElementById(EVENT_MODAL_CONTAINER_ID)
+
+        let event_cards = ""
+        let modal_cards = ""
+
+        this.events.forEach(event => {
+            event_cards += event.toEventCard()
+            modal_cards += event.toModal()
+        })
+
+        event_container.innerHTML = event_cards
+        modal_container.innerHTML = modal_cards
+    }
+}
+
+// Script Settings
+// Sets default values for the settings of this script if they've not already been set
+
+const EVENT_CARD_CONTAINER_ID = "event-card-container";
+const EVENT_MODAL_CONTAINER_ID = "event-modal-container";
+
+const SOURCES = [new CalendarSource("Queer Calendar Sheffield Google Calendar", CalendarSourceType.GOOGLE_CALENDAR, "queercalendarsheffield@gmail.com")];
+
+// Removed source, not enough info on the PQA calendar currently. Looking to add it back in if it's updated.
+// new CalendarSource("Peak Queer Adventures Google Calendar", SourceType.GOOGLE_CALENDAR, "peakqueeradventures@gmail.com")
+
+var loaded_sources = 0;
+
+// Instantiate a global calendar for the page that will be used to store info.
+var page_calendar = new Calendar(SOURCES);
+
+document.addEventListener("DOMContentLoaded", function () {
+    page_calendar.fetchAllEvents()
+    .then(() => {
+        page_calendar.outputInfo()
+    })
+})
