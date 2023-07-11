e9be6881423e67f794d2c913be730b23fc3b6ba6
CanopusFalling <kent_sam@outlook.com>
2023-07-11 18:42:42 +0100

    Added footer and cleaned up headder

--- app/footer.tsx
+++ app/footer.tsx
@@ -2,8 +2,23 @@ import React from 'react';
 
 export default function Footer() {
   return (
-    <footer>
-      {/* Your footer content goes here */}
+    <footer className="bg-neutral-100 text-center dark:bg-neutral-600 lg:text-left">
+      <div className="container p-6 text-neutral-800 dark:text-neutral-200">
+        <div className="mb-6 md:mb-0">
+          <p>
+            This website is built entirely by volenteers to advertise local queer events in Sheffield.
+          </p>
+        </div>
+      </div>
+
+      <div className="bg-neutral-200 p-4 text-center text-neutral-700 dark:bg-neutral-700 dark:text-neutral-200">
+        <div className="flex justify-center">
+          <a className="mr-4" href="email:queercalendarsheffield@gmail.com">Email Us</a>
+          <a className="mr-4" href="https://forms.gle/KE4iZaxnhtfw4RCF7">Add An Event</a>
+          <a className="mr-4" href="https://github.com/CanopusFalling/Queer-Calendar-Sheffield">View Project on Github</a>
+          {/* Add more links as needed */}
+        </div>
+      </div>
     </footer>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import './globals.css';
+
 import Header from './header';
 import Footer from './footer';
 
