cc8313f7838df69aac3eaad5668887aaf68ba9e6
CanopusFalling <kent_sam@outlook.com>
2023-07-26 18:54:53 +0100

    Trying to load NavLink with next/dynamic to see if that fixes the build.

--- app/components/navbar/adaptiveLinks.tsx
+++ app/components/navbar/adaptiveLinks.tsx
@@ -1,8 +1,13 @@
 'use client';
 
 import React from 'react';
+import dynamic from 'next/dynamic';
 
-import NavLink, { NavLinkProps } from './navLink'
+const NavLink = dynamic(() => import('./navLink'), {
+    ssr: false // This will ensure the component is only loaded on the client-side
+});
+
+import { NavLinkProps } from './navLink'
 import jsonNavLinks from './navigationLinks.json'
 const navLinks: NavLinkProps[] = jsonNavLinks;
 
@@ -10,25 +15,25 @@ interface AdaptiveLinksProps {
     isNavOpen: Boolean;
 }
 
-function AdaptiveLinks({isNavOpen}: AdaptiveLinksProps) {
+function AdaptiveLinks({ isNavOpen }: AdaptiveLinksProps) {
     return (
         <div
-          className={`${isNavOpen ? 'flex' : 'hidden'
-            } mt-2 lg:flex flex-grow basis-[100%] items-center lg:mt-0 lg:basis-auto`}
-          id="navbarSupportedContent3"
-          data-te-collapse-item
+            className={`${isNavOpen ? 'flex' : 'hidden'
+                } mt-2 lg:flex flex-grow basis-[100%] items-center lg:mt-0 lg:basis-auto`}
+            id="navbarSupportedContent3"
+            data-te-collapse-item
         >
-          <div
-            className="mt-4 list-style-none mr-auto flex flex-col pl-0 lg:mt-0 lg:ml-4 lg: lg:flex-row"
-          >
-            {navLinks.map((link, index) => (
-              <NavLink
-                key={index}
-                label={link.label}
-                href={link.href}
-              />
-            ))}
-          </div>
+            <div
+                className="mt-4 list-style-none mr-auto flex flex-col pl-0 lg:mt-0 lg:ml-4 lg: lg:flex-row"
+            >
+                {navLinks.map((link, index) => (
+                    <NavLink
+                        key={index}
+                        label={link.label}
+                        href={link.href}
+                    />
+                ))}
+            </div>
         </div>
     );
 }
