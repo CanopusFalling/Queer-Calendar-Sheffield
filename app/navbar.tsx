3f9b1483cbf4a73e7f8ba4958ddda4b4528c47db
CanopusFalling <kent_sam@outlook.com>
2023-07-26 14:21:12 +0100

    Added contributors to Navbar.

--- app/navbar.tsx
+++ app/navbar.tsx
@@ -1,13 +1,19 @@
-import React from 'react';
+'use client';
+
+import React, { useState } from 'react';
+
+interface HamburgerButtonProps {
+  onClick: () => void;
+}
 
 interface NavLinkProps {
   label: string;
   href: string;
-  isActive?: boolean;
 }
 
 const navLinks: NavLinkProps[] = [
-  { label: 'Home', href: '#', isActive: true },
+  { label: 'Home', href: '/'},
+  { label: 'Contributors', href: '/contributors'}
 ];
 
 function Logo() {
@@ -22,12 +28,13 @@ function Logo() {
 }
 
 
-function NavLink({ label, href, isActive }: NavLinkProps) {
+function NavLink({ label, href}: NavLinkProps) {
+  const isActive = window.location.pathname === href;
+
   return (
-    <div className="mb-4 pl-2 lg:mb-0 lg:pl-0 lg:pr-1" data-te-nav-item-ref>
+    <div className="mb-4 pl-2 lg:mb-0 lg:pl-0 lg:pr-1">
       <a
-        className={`p-0 text-neutral-500 transition duration-200 hover:text-neutral-700 hover:ease-in-out focus:text-neutral-700 disabled:text-black/30 motion-reduce:transition-none dark:text-neutral-200 dark:hover:text-neutral-400 dark:focus:text-neutral-400 lg:px-2 ${isActive ? '[&.active]:text-black/90 dark:[&.active]:text-neutral-400' : ''
-          }`}
+        className={`p-0  transition duration-200 hover:text-neutral-700 hover:ease-in-out focus:text-neutral-700 disabled:text-black/30 motion-reduce:transition-none  dark:hover:text-neutral-400 dark:focus:text-neutral-400 lg:px-2 ${isActive ? 'text-black/90 dark:text-neutral-400' : 'text-neutral-500 dark:text-neutral-200'}`}
         href={href}
         data-te-nav-link-ref
       >
@@ -51,42 +58,49 @@ function HamburgerSvg() {
   )
 }
 
-function HamburgerButton() {
+function HamburgerButton({ onClick }: HamburgerButtonProps) {
   return (
     <button
       className="block border-0 bg-transparent px-2 text-neutral-500 hover:no-underline hover:shadow-none focus:no-underline focus:shadow-none focus:outline-none focus:ring-0 dark:text-neutral-200 lg:hidden"
       type="button"
-      data-te-collapse-init
-      data-te-target="#navbarSupportedContent3"
-      aria-controls="navbarSupportedContent3"
-      aria-expanded="false"
-      aria-label="Toggle navigation">
+      onClick={onClick} // Add onClick handler
+      aria-label="Toggle navigation"
+    >
       <span className="[&>svg]:w-7">
         <HamburgerSvg />
       </span>
     </button>
-  )
+  );
 }
 
+
 export default function Navbar() {
+  const [isNavOpen, setIsNavOpen] = useState(false);
+
+  function handleToggleNav() {
+    setIsNavOpen(!isNavOpen);
+  }
+
   return (
     <nav className="relative flex w-full flex-nowrap items-center justify-between bg-[#FBFBFB] py-2 text-neutral-500 shadow-lg hover:text-neutral-700 focus:text-neutral-700 dark:bg-neutral-600 lg:flex-wrap lg:justify-start lg:py-4">
       <div className="flex w-full flex-wrap items-center justify-between px-3">
         <Logo />
-        <HamburgerButton />
+        <HamburgerButton onClick={handleToggleNav} />
         <div
-          className="!visible mt-2 hidden flex-grow basis-[100%] items-center lg:mt-0 lg:!flex lg:basis-auto"
+          className={`${
+            isNavOpen ? 'flex' : 'hidden'
+          } mt-2 lg:flex flex-grow basis-[100%] items-center lg:mt-0 lg:basis-auto`}
           id="navbarSupportedContent3"
-          data-te-collapse-item>
+          data-te-collapse-item
+        >
           <div
-            className="list-style-none mr-auto flex flex-col pl-0 lg:mt-1 lg:flex-row"
-            data-te-navbar-nav-ref>
+            className="mt-4 list-style-none mr-auto flex flex-col pl-0 lg:mt-0 lg:ml-4 lg: lg:flex-row"
+          >
             {navLinks.map((link, index) => (
               <NavLink
                 key={index}
                 label={link.label}
                 href={link.href}
-                isActive={index === 0}
               />
             ))}
           </div>
