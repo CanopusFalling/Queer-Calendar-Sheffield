beda5dd1355ec95edc0facfd934d1b4fa0445008
CanopusFalling <kent_sam@outlook.com>
2023-08-30 19:59:58 +0100

    Updated the description to be correct in the test file.

--- cypress/e2e/event.cy.ts
+++ cypress/e2e/event.cy.ts
@@ -12,7 +12,7 @@ const events: EventInfo[] = [
         title: "Rainbow Minds - LGBTQ+ social cafe",
         time: "30th August 2023 17:30 - 19:30",
         location: "Location: 110 Sharrow Ln, Highfield, Sheffield S11 8AL, UK",
-        description: "Held at the Wellbeing centre by sheffield mind. This LGBTQ+ social cafe is a weekly, informal support group, providing a safe space for people to meet, to talk to share and to support one another. Dan and his team will be on hand to make you feel welcome so why not come on over to our place and say hello. Email info@sheffieldmind.co.uk for more info"
+        description: "Held at the Wellbeing centre by sheffield mind. This LGBTQ+ social cafe is a weekly, informal support group, providing a safe space for people to meet, to talk to share and to support one another. Dan and his team will be on hand to make you feel welcome so why not come on over to our place and say hello.Email info@sheffieldmind.co.uk for more info"
     }
 ];
 
