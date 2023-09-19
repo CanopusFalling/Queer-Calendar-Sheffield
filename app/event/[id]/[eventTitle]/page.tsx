945471575d82c535826bcec4c0517f2ca5deebb8
CanopusFalling <kent_sam@outlook.com>
2023-09-19 22:58:59 +0100

    Added error page for if the event does not exist.

--- app/event/[id]/[eventTitle]/page.tsx
+++ app/event/[id]/[eventTitle]/page.tsx
@@ -5,11 +5,19 @@ import { redirect } from 'next/navigation';
 
 import { getEvent } from '../../getEvent'
 import EventCard from '../../eventCard';
+import EventNotFound from '../../event_not_found';
 
 export default async function Page({ params }: { params: { id: string, eventTitle: string }}) {
     const eventId = params.id;
 
-    const event = (await getEvent({ eventId: eventId }));
+    let event;
+
+    try {
+        event = (await getEvent({ eventId: eventId }));
+    } catch (error) {
+        console.log(error)
+        return (<EventNotFound/>);
+    }
 
     // Redirect the user if the title in the URL does not match.
     if(params.eventTitle != event.getURIEncodedName()){
--- app/event/[id]/page.tsx
+++ app/event/[id]/page.tsx
@@ -1,15 +1,22 @@
 export const runtime = 'edge';
-
 import React from 'react';
+
 import { redirect } from 'next/navigation';
 
 import { getEvent } from '../getEvent'
-import EventCard from '../eventCard';
+import EventNotFound from '../event_not_found';
 
 export default async function Page({ params }: { params: { id: string } }) {
     const eventId = params.id;
 
-    const event = (await getEvent({ eventId: eventId }));
+    let event;
+
+    try {
+        event = (await getEvent({ eventId: eventId }));
+    } catch (error) {
+        console.log(error)
+        return (<EventNotFound/>);
+    }
 
     redirect(event.getPath());
 }
