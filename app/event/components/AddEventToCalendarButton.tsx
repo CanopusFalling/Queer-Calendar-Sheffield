cb78ebbe106325f25acb9b8c57c8bd67f1ca5577
CanopusFalling <kent_sam@outlook.com>
2023-12-09 18:47:34 +0000

    Completed add to calendar functionality.

--- app/event/components/AddEventToCalendarButton.tsx
+++ app/event/components/AddEventToCalendarButton.tsx
@@ -13,15 +13,25 @@ interface AddEventToCalendarButtonProps {
 const AddEventToCalendarButton: React.FC<AddEventToCalendarButtonProps> = ({
   event,
 }) => {
+  const eventLink = window.location.origin + event.path;
   return (
     <AddToCalendarButton
+      //Event Details
       name={event.summary}
-      description={event.description}
-      options={["Apple", "Google"]}
+      description={`[url]${eventLink}|View this event on Queer Calendar Sheffield[/url]\n${event.description}`}
       startDate={moment(event.startTime).format("YYYY-MM-DD")}
       endDate={moment(event.endTime).format("YYYY-MM-DD")}
       startTime={moment(event.startTime).format("HH:mm")}
       endTime={moment(event.endTime).format("HH:mm")}
+      timeZone={moment.tz.guess()}
+      location={event.location}
+      //Button Settings
+      options={["Google", "Apple", "iCal", "Microsoft365", "Yahoo"]}
+      buttonStyle="default"
+      trigger="click"
+      hideCheckmark={true}
+      size="3"
+      lightMode="system"
     />
   );
 };
