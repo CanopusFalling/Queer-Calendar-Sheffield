afba319aa74be9c693298ebb46c4e1e55afa35c9
CanopusFalling <kent_sam@outlook.com>
2023-12-11 11:00:40 +0000

    Attempting og generation with next canary.

--- app/event/[eventId]/opengraph-image.tsx
+++ app/event/[eventId]/opengraph-image.tsx
@@ -1,23 +1,37 @@
+import { ImageResponse } from "next/og";
+
+// Route segment config
+export const runtime = "edge";
+
 // Image metadata
-export const alt = "Hello";
+export const alt = "Queer Calendar Sheffield";
 export const size = {
   width: 1200,
   height: 630,
 };
 
-export const contentType = "image/svg";
+export const contentType = "image/png";
 
 // Image generation
 export default async function Image() {
-  const svgContent = `
-    <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100">
-      <circle cx="50" cy="50" r="40" stroke="black" stroke-width="3" fill="red" />
-    </svg>
-  `;
-
-  return new Response(svgContent, {
-    headers: {
-      "Content-Type": "image/svg+xml",
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          fontSize: 128,
+          background: "white",
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        Queer Calendar Sheffield
+      </div>
+    ),
+    {
+      ...size,
     },
-  });
+  );
 }
