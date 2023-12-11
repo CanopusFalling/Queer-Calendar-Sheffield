4c03514da5643b94d286092c14b647543d5969b0
CanopusFalling <kent_sam@outlook.com>
2023-12-11 10:28:43 +0000

    Changed to generated metadata format.

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,53 +8,57 @@ import Footer from "./components/footer";
 
 import type { Metadata } from "next";
 
-let baseURL = "";
+export async function generateMetadata(): Promise<Metadata> {
+  let metadata: Metadata = {
+    title: "Queer Calendar Sheffield",
+    description: "Find out what queer events are on in Sheffield!",
+    openGraph: {
+      title: "Queer Calendar Sheffield",
+      type: "website",
+      images: [
+        {
+          url: "open-graph-banner.webp",
+          width: 1200,
+          height: 630,
+          alt: 'A rainbow calendar icon with a heart shape in the centre of the icon and to the right of that the words "Queer Calendar Sheffield" in white, bold text each word on a new line all on a black background.',
+        },
+      ],
+      siteName: "Queer Calendar Sheffield",
+      locale: "en_GB",
+      description: "A place to find queer and LGBTQ+ events in Sheffield.",
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: "Queer Calendar Sheffield",
+      description: "A place to find queer and LGBTQ+ events in Sheffield.",
+      images: [
+        {
+          url: "open-graph-banner.webp",
+          width: 1200,
+          height: 630,
+          alt: 'A rainbow calendar icon with a heart shape in the centre of the icon and to the right of that the words "Queer Calendar Sheffield" in white, bold text each word on a new line all on a black background.',
+        },
+      ],
+    },
+    icons: {
+      icon: "./favicon.ico",
+    },
+  };
 
-if (process.env.CF_PRODUCTION == "1") {
-  baseURL = "https://queercalendarsheffield.co.uk";
-} else {
-  baseURL = process.env.CF_PAGES_URL as string;
+  //Set a baseURL only if the environment is a production Env.
+  if (process.env.CF_PRODUCTION == "1") {
+    metadata.metadataBase = new URL("https://queercalendarsheffield.co.uk");
+    console.log("Production Layout");
+  } else if (process.env.CF_PAGES_URL) {
+    metadata.metadataBase = new URL(process.env.CF_PAGES_URL as string);
+    console.log("CF deploy development layout.");
+  } else {
+    metadata.metadataBase = new URL("https://queercalendarsheffield.co.uk");
+    console.log("Local dev layout");
+  }
+
+  return metadata;
 }
-
-console.log(`CF PAGES URL ${process.env.CF_PAGES_URL}`);
-
-export const metadata: Metadata = {
-  metadataBase: baseURL == "" ? undefined : new URL(baseURL),
-  title: "Queer Calendar Sheffield",
-  description: "Find out what queer events are on in Sheffield!",
-  openGraph: {
-    title: "Queer Calendar Sheffield",
-    url: "/",
-    type: "website",
-    images: [
-      {
-        url: "open-graph-banner.webp",
-        width: 1200,
-        height: 630,
-        alt: 'A rainbow calendar icon with a heart shape in the centre of the icon and to the right of that the words "Queer Calendar Sheffield" in white, bold text each word on a new line all on a black background.',
-      },
-    ],
-    siteName: "Queer Calendar Sheffield",
-    locale: "en_GB",
-    description: "A place to find queer and LGBTQ+ events in Sheffield.",
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: "Queer Calendar Sheffield",
-    description: "A place to find queer and LGBTQ+ events in Sheffield.",
-    images: [
-      {
-        url: "open-graph-banner.webp",
-        width: 1200,
-        height: 630,
-        alt: 'A rainbow calendar icon with a heart shape in the centre of the icon and to the right of that the words "Queer Calendar Sheffield" in white, bold text each word on a new line all on a black background.',
-      },
-    ],
-  },
-  icons: {
-    icon: "./favicon.ico",
-  },
-};
 
 export default function RootLayout({
   children,
