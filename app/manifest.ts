ee79fbe623123b78a01497444bd98eb307dd4e0a
CanopusFalling <kent_sam@outlook.com>
2023-12-28 16:47:05 +0000

    Moved to generated manifest using nextjs manifest.ts file.

---
+++ app/manifest.ts
@@ -0,0 +1,30 @@
+import { MetadataRoute } from "next";
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: "Queer Calendar Sheffield",
+    short_name: "QCS",
+    description: "A place to find queer and LGBTQ+ events in Sheffield",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#000",
+    theme_color: "#000",
+    icons: [
+      {
+        src: "/favicon.ico",
+        sizes: "any",
+        type: "image/x-icon",
+      },
+      {
+        src: "/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/android-chrome-256x256.png",
+        sizes: "256x256",
+        type: "image/png",
+      },
+    ],
+  };
+}
