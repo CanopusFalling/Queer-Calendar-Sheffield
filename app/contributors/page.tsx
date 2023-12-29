6e89f1f83b0121c42c21e1fca851c0667a88d82d
CanopusFalling <kent_sam@outlook.com>
2023-12-29 16:15:11 +0000

    More changes to styling.

--- app/contributors/page.tsx
+++ app/contributors/page.tsx
@@ -4,7 +4,7 @@ import Contributors from "./contributors";
 const CreditsPage: React.FC = () => {
   return (
     <main className="mb-32 m-8 text-center">
-      <h2 className="mb-6 text-3xl font-bold bg-white p-4 rounded-xl">
+      <h2 className="mb-6 text-3xl font-bold bg-white dark:bg-neutral-700 p-4 rounded-xl">
         Project Contributors
       </h2>
       <Contributors />
