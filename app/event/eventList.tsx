ad71e3f597a5895f4a66d8ee496065eda7f48fa7
CanopusFalling <kent_sam@outlook.com>
2023-12-31 16:56:55 +0000

    Added rich result for EventList.

--- app/event/eventList.tsx
+++ app/event/eventList.tsx
@@ -9,9 +9,31 @@ interface EventListProps {
 }
 
 export default async function EventList({ events }: EventListProps) {
-  //console.log(events)
+  const jsonLd = {
+    "@context": "http://schema.org",
+    "@type": "ItemList",
+    name: "Event List",
+    numberOfItems: events.length,
+    itemListElement: events.map((event, index) => ({
+      "@type": "ListItem",
+      position: index + 1,
+      item: {
+        "@type": "Event",
+        name: event.summary,
+        startDate: event.startTime.toDateString(),
+        endDate: event.endTime.toDateString(),
+        location: event.location,
+        description: event.description,
+      },
+    })),
+  };
+
   return (
     <>
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+      />
       {events.map((event) => (
         <EventCard key={event.id} event={event} linkEvent={true} />
       ))}
