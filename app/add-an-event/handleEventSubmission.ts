6961881f9709c1a40760b39a25c2befa2c8d19de
CanopusFalling <kent_sam@outlook.com>
2024-01-30 11:47:09 +0000

    Updated to print event info out in an okay format.

--- app/add-an-event/handleEventSubmission.ts
+++ app/add-an-event/handleEventSubmission.ts
@@ -1,5 +1,68 @@
 "use server";
 
+const zepto_url = process.env.ZEPTO_MAIL_URL;
+const zepto_token = process.env.ZEPTO_MAIL_TOKEN;
+
+const event_submission_email = process.env.EVENT_FORM_EMAIL;
+
 export default async function HandleEventSubmission(formData: FormData) {
-  console.log(formData);
+  console.log(formDataToHTML(formData));
+
+  if (zepto_token && zepto_url && event_submission_email) {
+    await MailEventInfo(formData);
+  } else {
+    console.log(
+      "No Transactional Mail Provider, or provided email, email not sent.",
+    );
+  }
+}
+
+function formDataToHTML(formData: FormData): string {
+  let htmlString = "<div><b>Form Data:</b></div><br>";
+
+  for (const [name, value] of formData.entries()) {
+    htmlString += `<div><b>${name}:</b> ${value}</div>`;
+  }
+
+  return htmlString;
+}
+
+async function MailEventInfo(formData: FormData) {
+  const apiUrl = `https://${zepto_url}/v1.1/email`; // Adjust the URL according to your API
+
+  const requestBody = {
+    from: {
+      address: "noreply@queercalendarsheffield.co.uk",
+      name: "noreply",
+    },
+    to: [
+      {
+        email_address: {
+          address: "contact@queercalendarsheffield.co.uk",
+          name: "Queer Calendar Sheffield",
+        },
+      },
+    ],
+    subject: "Event Form Submitted",
+    htmlbody: formDataToHTML(formData),
+  };
+
+  const headers: Record<string, string> = {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+    Authorization: zepto_token as string,
+  };
+
+  const response = await fetch(apiUrl, {
+    method: "POST",
+    headers: headers,
+    body: JSON.stringify(requestBody),
+  });
+
+  if (response.ok) {
+    const responseData = await response.json();
+    console.log("Email sent successfully:", responseData);
+  } else {
+    console.error("Failed to send email:", response.statusText);
+  }
 }
