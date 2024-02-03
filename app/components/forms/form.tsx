7451be06b9841f20e6e6917ca9fbf37bca7e7257
CanopusFalling <kent_sam@outlook.com>
2024-02-03 23:54:34 +0000

    Finished Event Form Formatting and fields.

--- app/components/forms/form.tsx
+++ app/components/forms/form.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import { InputProps } from "tw-elements-react/dist/types/forms/Input/types";
 import { TEInput, TERipple, TETextarea } from "tw-elements-react";
 import DateTimePicker from "./datetimePicker";
+import Checkbox from "./checkbox";
 
 interface FormProps {
   inputFields: { props: React.FC<InputProps>; description?: string }[];
@@ -14,7 +15,7 @@ const Form: React.FC<FormProps> = ({ inputFields, submitAction }) => {
   return (
     <form
       action={submitAction}
-      className="flex flex-col gap-2 rounded-lg bg-white p-6 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700"
+      className="flex flex-col gap-2 rounded-lg bg-white p-6 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-900"
     >
       {inputFields.map((field, key) => (
         <div key={key}>
@@ -22,12 +23,20 @@ const Form: React.FC<FormProps> = ({ inputFields, submitAction }) => {
             <DateTimePicker {...(field.props as InputProps)} />
           ) : (field.props as InputProps).type === "textarea" ? (
             <TETextarea {...(field.props as any)} />
+          ) : (field.props as InputProps).type === "checkbox" ? (
+            <Checkbox {...(field.props as any)} />
           ) : (
             <TEInput {...(field.props as InputProps)} />
           )}
 
+          {(field.props as InputProps).required && (
+            <span className="font-bold not-italic text-warning text-sm mx-2">
+              Required{" "}
+            </span>
+          )}
+
           {field.description && (
-            <div className="italic text-sm px-2">{field.description}</div>
+            <div className="italic text-sm mx-2 mb-2">{field.description}</div>
           )}
         </div>
       ))}
@@ -35,7 +44,7 @@ const Form: React.FC<FormProps> = ({ inputFields, submitAction }) => {
       <TERipple rippleColor="light">
         <button
           type="submit"
-          className="inline-block rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
+          className="inline-block rounded bg-primary px-6 pb-2 pt-2 text-l w-full font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
         >
           Submit
         </button>
