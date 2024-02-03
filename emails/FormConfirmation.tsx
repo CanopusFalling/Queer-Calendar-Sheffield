bd754db03eb627486a70cfbba4a21f5a8d7eb6b1
CanopusFalling <kent_sam@outlook.com>
2024-02-03 20:06:55 +0000

    Minor Form Tweaks.

--- emails/FormConfirmation.tsx
+++ emails/FormConfirmation.tsx
@@ -9,18 +9,22 @@ import {
   Text,
   Link,
   Body,
+  Img,
+  Preview,
 } from "@react-email/components";
 
 import { BsInstagram, BsFacebook, BsEnvelopeFill } from "react-icons/bs";
 
 interface FormConfirmationProps {
   title: string;
+  preview: string;
   description: string;
   formData: Record<string, any>;
 }
 
 export function FormConfirmation({
   title,
+  preview,
   description,
   formData,
 }: FormConfirmationProps) {
@@ -41,8 +45,14 @@ export function FormConfirmation({
           <Head>
             <title>{title}</title>
           </Head>
+          <Preview>{preview}</Preview>
           <Container className="max-w-screen-md bg-black rounded-xl p-10 text-white">
-            <Heading as="h1" className="text-xxl m-0">
+            <Img
+              src="https://queercalendarsheffield.co.uk/icon/128.webp"
+              alt="Queer Calendar Sheffield Logo"
+              className="h-20 mx-auto mb-6"
+            />
+            <Heading as="h1" className="text-xxl m-0 mb-4 text-center">
               {title}
             </Heading>
             <Hr />
@@ -104,7 +114,8 @@ export function FormConfirmation({
 }
 
 FormConfirmation.PreviewProps = {
-  title: "Event Submission Confirmation",
+  title: "Thank You For Submitting Your Event!",
+  preview: "We have recieved your event and will let you know when it's live!",
   description: "Thank you for submitting your event!",
   formData: {
     Title: "Bouldering @ Climbing Hangar",
