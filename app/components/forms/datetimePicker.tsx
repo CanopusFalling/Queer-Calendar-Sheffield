663583c9f182ab48d1e6b2b191b87f20ebfc6e15
CanopusFalling <kent_sam@outlook.com>
2024-02-03 22:39:47 +0000

    Updated datetime picker to use required prop.

--- app/components/forms/datetimePicker.tsx
+++ app/components/forms/datetimePicker.tsx
@@ -3,20 +3,7 @@
 import React from "react";
 import { InputProps } from "tw-elements-react/dist/types/forms/Input/types";
 
-const DateTimePicker: React.FC<InputProps> = ({
-  label,
-  labelRef,
-  labelID,
-  ref,
-  readOnly,
-  disabled,
-  size,
-  wrapperTag,
-  theme,
-  formWhite,
-  counter,
-  maxLength,
-}) => {
+const DateTimePicker: React.FC<InputProps> = ({ label, required }) => {
   return (
     <div className="flex flex-row items-center justify-between">
       <label htmlFor={label as string} className="flex-shrink-0 m-2">
@@ -25,6 +12,7 @@ const DateTimePicker: React.FC<InputProps> = ({
       <input
         type="datetime-local"
         name={label as string}
+        required={required}
         className="min-h-[auto] w-full rounded border border-neutral-500 bg-transparent px-3 py-[0.32rem] leading-[1.6] dark:text-neutral-200 dark:placeholder:text-neutral-200"
       />
     </div>
