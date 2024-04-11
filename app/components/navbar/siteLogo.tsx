33646a4bf962081eaaa70e14eefc1c2f2b32bf5c
CanopusFalling <kent_sam@outlook.com>
2024-04-11 18:01:37 +0100

    Changed brightness for light mode readablility.

--- app/components/navbar/siteLogo.tsx
+++ app/components/navbar/siteLogo.tsx
@@ -4,7 +4,7 @@ import React from "react";
 function SiteLogo() {
   return (
     <Link
-      className="flex p-3 pt-2 text-4xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-green-300 via-blue-400 to-purple-500 brightness-80 dark:brightness-110"
+      className="flex p-3 pt-2 text-4xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-green-300 via-blue-400 to-purple-500 brightness-50 dark:brightness-110"
       href="/"
       aria-label="Homepage"
     >
