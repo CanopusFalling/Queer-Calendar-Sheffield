2d4f650c85304e6ea4555c86dae6dba275579fc7
CanopusFalling <kent_sam@outlook.com>
2024-04-11 22:57:23 +0100

    Better testing data for the overtflow scroll.

--- app/components/overflow-scroll/overflowScroll.tsx
+++ app/components/overflow-scroll/overflowScroll.tsx
@@ -4,8 +4,21 @@ export interface OverflowScrollProps {
 
 export default function OverflowScroll({ title }: OverflowScrollProps) {
   return (
-    <div className="flex flex-row overflow-x-scroll scroll-smooth m-2 gap-4 snap-x">
-      <div className="p-10 bg-pink-700 rounded-xl"> hello! </div>
+    <div className="flex flex-row overflow-x-scroll scroll-smooth p-2 gap-4 snap-x">
+      <button className="p-10 bg-pink-700 rounded-xl"> hello! </button>
+      <button className="p-10 bg-pink-700 rounded-xl"> hello! </button>
+      <button className="p-10 bg-pink-700 rounded-xl"> hello! </button>
+      <button className="p-10 bg-pink-700 rounded-xl"> hello! </button>
+      <button className="p-10 bg-pink-700 rounded-xl"> hello! </button>
+      <button className="p-10 bg-pink-700 rounded-xl"> hello! </button>
+      <button className="p-10 bg-pink-700 rounded-xl"> hello! </button>
+      <button className="p-10 bg-pink-700 rounded-xl"> hello! </button>
+      <button className="p-10 bg-pink-700 rounded-xl"> hello! </button>
+      <button className="p-10 bg-pink-700 rounded-xl"> hello! </button>
+      <button className="p-10 bg-pink-700 rounded-xl"> hello! </button>
+      <button className="p-10 bg-pink-700 rounded-xl"> hello! </button>
+      <button className="p-10 bg-pink-700 rounded-xl"> hello! </button>
+      <button className="p-10 bg-pink-700 rounded-xl"> hello! </button>
     </div>
   );
 }
