10153cacfd0bc14bac709c4a5cdc77e6c43edf6f
CanopusFalling <kent_sam@outlook.com>
2024-04-11 21:09:54 +0100

    Updated footer.

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -4,39 +4,15 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer className="bg-neutral-100 text-center dark:bg-neutral-700 lg:text-left">
-      <div className="p-6">
-        <p className="text-center">
-          This calendar is built and maintained by{" "}
-          <Link className="underline" href="/contributors">
-            our contributors
-          </Link>
-          . You can also get involved below.
-        </p>
-      </div>
-
-      <div className="bg-white p-4 text-center dark:bg-neutral-600">
-        <div className="flex flex-col items-center sm:flex-row sm:justify-center">
-          <Link className="mr-4 mb-2 sm:mb-0" href="/contact">
-            Contact Us
-          </Link>
-          <Link className="mr-4 mb-2 sm:mb-0" href="/contact">
-            Add An Event
-          </Link>
-          <Link
-            className="mr-4 mb-2 sm:mb-0"
-            href="https://github.com/CanopusFalling/Queer-Calendar-Sheffield"
-          >
-            View Project on GitHub
-          </Link>
-          <Link className="mr-4 mb-2 sm:mb-0" href="/contributors">
-            Project Contributors
-          </Link>
-          <Link className="mr-4 mb-2 sm:mb-0" href="/accessibility">
-            Accessibility Statement
-          </Link>
-          {/* Add more links as needed */}
-        </div>
+    <footer className="bg-gradient-to-r from-green-300 via-blue-400 to-purple-500">
+      <div className="flex flex-col flex-wrap justify-center gap-4 sm:flex-row p-4 underline text-center bg-white/[0.8] dark:bg-black/[0.8]">
+        <Link href="/contact">Contact Us</Link>
+        <Link href="/contact">Add An Event</Link>
+        <Link href="https://github.com/CanopusFalling/Queer-Calendar-Sheffield">
+          View Project on GitHub
+        </Link>
+        <Link href="/contributors">Project Contributors</Link>
+        <Link href="/accessibility">Accessibility Statement</Link>
       </div>
     </footer>
   );
