e64c009121ca9d4c11c961be3843b87fefecf29d
CanopusFalling <kent_sam@outlook.com>
2024-04-17 12:52:25 +0100

    Starting work on redesigning events page. Might move to MDX for all relatively static pages to simplify changes in the future.

--- app/event/page.tsx
+++ app/event/page.tsx
@@ -24,6 +24,16 @@ export default async function EventPage(req: any, res: any) {
     };
     const events = await getEvents(eventParams);
 
-    return <EventList events={events} />;
+    return (
+      <>
+        <div className="mx-4">
+          <h1 className="mb-2 text-3xl font-bold bg-gradient-to-r">
+            Upcoming Events
+          </h1>
+          <hr className="mb-3 border-0 w-full h-px bg-gradient-to-r from-green-300 via-blue-400 to-purple-500 brightness-75 dark:brightness-100" />
+        </div>
+        <EventList events={events} />
+      </>
+    );
   }
 }
