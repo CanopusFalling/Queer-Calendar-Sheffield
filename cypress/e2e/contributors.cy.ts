5913119b89c792edcfbeb1372d9493a4b2cfbf7b
CanopusFalling <kent_sam@outlook.com>
2024-05-05 01:37:23 +0100

    Removed e-to-e for contributors while data is changing. I need to rethink how to add this testing in future.

--- cypress/e2e/contributors.cy.ts
+++ cypress/e2e/contributors.cy.ts
@@ -1,31 +1,31 @@
-// I would like to change these test to use the data over in cypress/fixtures/stubbed-contributors.json but for now this will do.
+// // I would like to change these test to use the data over in cypress/fixtures/stubbed-contributors.json but for now this will do.
 
-describe(`contributors`, () => {
-  const contributorPagePath = "/contributors";
+// describe(`contributors`, () => {
+//   const contributorPagePath = "/contributors";
 
-  beforeEach(() => {
-    cy.visit(contributorPagePath);
-  });
+//   beforeEach(() => {
+//     cy.visit(contributorPagePath);
+//   });
 
-  it(`should show page title`, () => {
-    cy.get("main").contains("Project Contributors");
-  });
+//   it(`should show page title`, () => {
+//     cy.get("main").contains("Project Contributors");
+//   });
 
-  it(`should show Sammy`, () => {
-    const details = ["Sammy", "They/Them", "Website Developer & Maintainer"];
-    details.forEach((detail) => {
-      cy.get("#contributor-0").contains(detail);
-    });
-  });
+//   it(`should show Sammy`, () => {
+//     const details = ["Sammy", "They/Them", "Website Developer & Maintainer"];
+//     details.forEach((detail) => {
+//       cy.get("#contributor-0").contains(detail);
+//     });
+//   });
 
-  it(`should have links to Sammy's Email, GitHub and Website`, () => {
-    const badgeInfo = [
-      ["mail-badge", "mailto:kent_sam@outlook.com"],
-      ["github-badge", "https://github.com/CanopusFalling"],
-      ["website-badge", "https://canopusfalling.co.uk"],
-    ];
-    badgeInfo.forEach((badge) => {
-      cy.get(`.${badge[0]}`).should("have.attr", "href", badge[1]);
-    });
-  });
-});
+//   it(`should have links to Sammy's Email, GitHub and Website`, () => {
+//     const badgeInfo = [
+//       ["mail-badge", "mailto:kent_sam@outlook.com"],
+//       ["github-badge", "https://github.com/CanopusFalling"],
+//       ["website-badge", "https://canopusfalling.co.uk"],
+//     ];
+//     badgeInfo.forEach((badge) => {
+//       cy.get(`.${badge[0]}`).should("have.attr", "href", badge[1]);
+//     });
+//   });
+// });
